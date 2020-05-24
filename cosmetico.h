ff8bd0711276de406d269e1646cd08c5ae6087da
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-24 20:23:05 +0200

    aggiunto header cosmetico

---
+++ cosmetico.h
@@ -0,0 +1,23 @@
+#ifndef COSMETICO_H
+#define COSMETICO_H
+
+using std::string;
+
+enum Sesso { "Uomo",
+             "Donna",
+             "Unisex" };
+
+class Cosmetico {
+private:
+  Sesso _genere;
+  string _applicazione;
+public:
+
+  virtual Cosmetico(unsigned short id, string nome, string descrizione, double prezzo, Sesso genere, string applicazione);
+  virtual Cosmetico* clone();
+  virtual double calcoloPrezzo();
+  virtual string mostraCosmetico();
+  virtual ~Cosmetico();
+};
+
+#endif // COSMETICO_H
