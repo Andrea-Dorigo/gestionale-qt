c1d21de708eb63e3018a30d6eb35ad2b28727288
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-25 18:12:20 +0200

    rimossa implementazione dei metodi SmartP dentro header

--- magazzino.h
+++ magazzino.h
@@ -7,25 +7,18 @@ class Magazzino {
 private:
   class SmartP {
   public:
-    ICTstaff* p;
-    SmartP(Prodotto* q=nullptr): p(q !=nullptr ? q->clone() : nullptr) {}
-    SmartP(const SmartP& s): p(s.p !=nullptr ? (s.p)->clone() : nullptr) {}
-    SmartP& operator=(const SmartP& s) {
-      if(this != &s) {
-	if(p) delete p;
-	p = s.p !=nullptr ? (s.p)->clone() : nullptr;
-      }
-      return *this;
-    }
-    ~SmartP() {if(p) delete p;}
-    Prodotto* operator->() const {return p;}
-    Prodotto& operator*() const {return *p;}
+    Prodotto* p;
+    SmartP(Prodotto* q=nullptr);
+    SmartP(const SmartP& s);
+    SmartP& operator=(const SmartP& s);
+    ~SmartP();
+    Prodotto* operator->() const;
+    Prodotto& operator*() const;
   };
   std::list<SmartP> sp;
 public:
   Magazzino();
   void MostraTutto();
 };
-
 
 #endif // MAGAZZINO_H
