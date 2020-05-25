eaab2f5511940c29f0fe549ada88945171a1f782
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-25 18:12:51 +0200

    aggiunta implementazione metodi

--- magazzino.cpp
+++ magazzino.cpp
@@ -1,3 +1,26 @@
 #include "magazzino.h"
 
-using std::string;
+Magazzino::SmartP::SmartP(Prodotto* q=nullptr): p(q!=nullptr ? q->clone() : nullptr) {}
+Magazzino::SmartP::SmartP(const SmartP& s): p(s.p !=nullptr ? (s.p)->clone() : nullptr) {}
+SmartP& Magazzino::SmartP::operator=(const SmartP& s) {
+  if(this != &s) {
+    if(p) delete p;
+    p = s.p !=nullptr ? (s.p)->clone() : nullptr;
+  }
+  return *this;
+}
+Magazzino::SmartP::~SmartP() {
+  if(p) delete p;
+}
+Prodotto* Magazzino::SmartP::operator->() const {
+  return p::
+}
+Prodotto& Magazzino::SmartP::operator*() const {
+  return *p;
+}
+Magazzino::Magazzino() {
+
+}
+void Magazzino::mostraTutto() {
+  
+}
