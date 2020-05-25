8a6c49ba01158e18646855d91682a973d087e13b
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-25 10:41:42 +0200

    cancellati commenti

--- prodotto.h
+++ prodotto.h
@@ -10,13 +10,9 @@ private:
   string _descrizione;
   double _prezzo;
 public:
-  //costruttore
   virtual Prodotto(unsigned short id, string nome, string descrizione, double prezzo=0.0);
-  //clone
   virtual Prodotto* clone() const=0;
-  //calcola il prezzo del prodotto
   virtual double calcoloPrezzo() const=0;
-  //
   virtual string mostraProdotto() const=0;
   virtual ~Prodotto();
 };
