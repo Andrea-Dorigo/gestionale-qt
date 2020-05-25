af00e3109a9cd9a3c733c2ed054358188bf069ce
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-25 10:47:08 +0200

    aggiunti spazi nel codice

--- prodotto.cpp
+++ prodotto.cpp
@@ -2,4 +2,4 @@
 
 using std::string;
 
-Prodotto::Prodotto(unsigned short id, string nome, string descrizione, double prezzo): _id(id),_nome(nome),_descrizione(descrizione),_prezzo(prezzo){}
+Prodotto::Prodotto(unsigned short id, string nome, string descrizione, double prezzo) : _id(id), _nome(nome), _descrizione(descrizione), _prezzo(prezzo){}
