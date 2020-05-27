c5dcb1a2e92fb1eb0e1905696c9189b5e8e98e43
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-27 17:19:21 +0200

    Abbellito il codice

--- prodotto.cpp
+++ prodotto.cpp
@@ -1,22 +1,42 @@
 #include "prodotto.h"
 
-Prodotto::Prodotto(unsigned short id, std::string nome, std::string descrizione, double prezzo) :
-_id(id),
-_nome(nome),
-_descrizione(descrizione),
-_prezzo(prezzo) {}
+Prodotto::Prodotto(unsigned short id, std::string nome, std::string descrizione, double prezzo)
+  : _id(id)
+  , _nome(nome)
+  , _descrizione(descrizione)
+  , _prezzo(prezzo)
+  {}
+
 Prodotto::~Prodotto() {}
 
-// Prodotto* Prodotto::clone() const {}
-// double Prodotto::calcoloPrezzo() const {}
-// std::string Prodotto::mostraProdotto() const {}
+// Prodotto* Prodotto::clone() const {} //=0
+// double Prodotto::calcoloPrezzo() const {} //=0
+// std::string Prodotto::mostraProdotto() const {} //=0
 
-unsigned short Prodotto::getId() const {return _id;}
-std::string Prodotto::getNome() const {return _nome;}
-std::string Prodotto::getDescrizione() const {return _descrizione;}
-double Prodotto::getPrezzo() const {return _prezzo;}
+/* getters */
+unsigned short Prodotto::getId() const {
+  return _id;
+}
+std::string Prodotto::getNome() const {
+  return _nome;
+}
+std::string Prodotto::getDescrizione() const {
+  return _descrizione;
+}
+double Prodotto::getPrezzo() const {
+  return _prezzo;
+}
 
-void Prodotto::setId(unsigned short id){_id = id;}
-void Prodotto::setNome(std::string nome){_nome = nome;}
-void Prodotto::setDescrizione(std::string descrizione){_descrizione = descrizione;}
-void Prodotto::setPrezzo(double prezzo){_prezzo = prezzo;}
+/* setters */
+void Prodotto::setId(unsigned short id) {
+  _id = id;
+}
+void Prodotto::setNome(std::string nome) {
+  _nome = nome;
+}
+void Prodotto::setDescrizione(std::string descrizione) {
+  _descrizione = descrizione;
+}
+void Prodotto::setPrezzo(double prezzo) {
+  _prezzo = prezzo;
+}
