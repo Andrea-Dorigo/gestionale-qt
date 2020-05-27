2f9856f9eb569324e9bb28592cdda82641395286
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-27 17:22:10 +0200

    Aggiunto commenti getters, setters

--- prodotto.h
+++ prodotto.h
@@ -12,16 +12,15 @@ private:
 public:
   Prodotto(unsigned short id, std::string nome, std::string descrizione, double prezzo=0.0);
   virtual ~Prodotto();
-
   virtual Prodotto* clone() const=0;
   virtual double calcoloPrezzo() const=0;
   virtual std::string mostraProdotto() const=0;
-
+  /* getters */
   unsigned short getId() const;
   std::string getNome() const;
   std::string getDescrizione() const;
   double getPrezzo() const;
-
+  /* setters */
   void setId(unsigned short id);
   void setNome(std::string nome);
   void setDescrizione(std::string descrizione);
