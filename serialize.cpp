76e4f93fb04ac68b913d7967457d5d19578f380f
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-28 15:19:31 +0200

    Modificata implementazione read(), write()

--- serialize.cpp
+++ serialize.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <iomanip>
 #include "nlohmann/json.hpp"
 #include "serialize.h"
 
-// read a JSON file
-void Serialize::read() {
-  std::ifstream i("file.json");
+// read a JSON file and return a json element
+nlohmann::json Serialize::read(const std::string& nome_file) {
+  std::ifstream i(nome_file);
   nlohmann::json j;
   i >> j;
+  return j;
 }
 
-// write prettified JSON to another file
-void Serialize::write() {
-  std::ofstream o("pretty.json");
-  nlohmann::json j;
+// write prettified JSON to file
+void Serialize::write(const std::string& nome_file, const nlohmann::json& j) {
+  std::ofstream o(nome_file);
   o << std::setw(4) << j << std::endl;
 }
 
 int main() {
   Serialize s;
-  s.write();
+  std::string nome_file = "magazzino.json";
+  nlohmann::json documento;
+
+  /* scrive il contenuto di documento nel file .json
+  documento["nome"] = "Mario";
+  documento["cognome"] = "Rossi";
+  s.write(nome_file, documento);
+  */
+
+  /* legge il file .json e lo salva in un elemento js
+  documento = s.read(nome_file);
+  std::cout << std::setw(4) << documento << std::endl;
+  */
+
   return 0;
 }
--- serialize.h
+++ serialize.h
@@ -1,10 +1,13 @@
 #ifndef SERIALIZE_H
 #define SERIALIZE_H
 
+#include <string>
+#include "nlohmann/json.hpp"
+
 class Serialize {
 public:
-  void read();
-  void write();
+  nlohmann::json read(const std::string& nome_file);
+  void write(const std::string& nome_file, const nlohmann::json& j);
 };
 
 #endif // SERIALIZE_H
