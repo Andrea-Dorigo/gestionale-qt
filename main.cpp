3cef1cfb0071db983171618d0df9bc07437113f5
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-28 03:20:40 +0200

    Aggiunto esempio concreto json

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include "cosmetico.h"
-
+#include "nlohmann/json.hpp"
 
 int main() {
+  /* costruttori a cui fare riferimento
+       Prodotto(unsigned short id, std::string nome, std::string descrizione, double prezzo=0.0);
+       Cosmetico(Target target, std::string applicazione);
+  */
+
   Cosmetico c(1, "NomeProdotto", "DescrizioneProdotto", 10.50, UOMO, "ApplicazioneCosmetico");
-  std::cout << c.mostraProdotto() <<std::endl;
+  std::cout << c.mostraProdotto() << std::endl;
+
+  //Cosmetico c(1, "nome", "descrizione", 0.5, DONNA, "applicazione");
+  //nlohmann::json documento = c; //non funziona anche se dovrebbe
+  //c.to_json(documento, c);
+  //std::cout << documento << std::endl;
+
   return 0;
 }
