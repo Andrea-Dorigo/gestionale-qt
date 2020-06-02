267f3f02ef60b47a315d9c4de8edff8acca94580
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-02 21:50:12 +0200

    Indentato il codice

--- Container/magazzino.h
+++ Container/magazzino.h
@@ -6,17 +6,15 @@
 #include "prodotto.h"
 
 class Magazzino {
-private:
-  std::list<SmartP> sp;
-public:
-  //inserisce un prodotto nel magazzino
-  bool insert(Prodotto* p);
-
-  //elimina il prodotto con un certo id
-  bool deleteProdotto(int id);
-
-  //funzione da rimuovere da qui
-  bool mostraProdotto(Prodotto* p);
+  private:
+    std::list<SmartP> sp;
+  public:
+    //inserisce un prodotto nel magazzino
+    bool insert(Prodotto* p);
+    //elimina il prodotto con un certo id
+    bool deleteProdotto(int id);
+    //funzione da rimuovere da qui
+    bool mostraProdotto(Prodotto* p);
 };
 
 #endif // MAGAZZINO_H
