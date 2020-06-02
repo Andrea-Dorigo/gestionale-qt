13fda3d34607f06a6daee569cdb143e51b4dd1d1
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-02 16:51:06 +0200

    Indentato il codice

--- cosmetico.h
+++ cosmetico.h
@@ -16,26 +16,26 @@ std::string attributeToString(Target t);
 */
 
 class Cosmetico : public Prodotto {
-private:
-  Target _target;
-  std::string _applicazione;
-public:
-  Cosmetico(unsigned short id, std::string nome, std::string descrizione, double prezzo, Target target, std::string applicazione);
-  virtual ~Cosmetico();
-  virtual Cosmetico* clone() const;
-  virtual double calcoloPrezzo() const;
-  // Cosmetico& operator*() const;
-  /* da spostare in classe serializzazione */
-  // virtual std::string mostraProdotto() const;
-  // std::string targetToString(Target target) const;
-  // void to_json(nlohmann::json& j, const Cosmetico& p);
+  private:
+    Target _target;
+    std::string _applicazione;
+  public:
+    Cosmetico(unsigned short id, std::string nome, std::string descrizione, double prezzo, Target target, std::string applicazione);
+    virtual ~Cosmetico();
+    virtual Cosmetico* clone() const;
+    virtual double calcoloPrezzo() const;
+    // Cosmetico& operator*() const;
+    /* da spostare in classe serializzazione */
+    // virtual std::string mostraProdotto() const;
+    // std::string targetToString(Target target) const;
+    // void to_json(nlohmann::json& j, const Cosmetico& p);
 
-  /* getters */
-  Target getTarget() const;
-  std::string getApplicazione() const;
-  /* setters */
-  void setTarget(Target target);
-  void setApplicazione(std::string applicazione);
+    /* getters */
+    Target getTarget() const;
+    std::string getApplicazione() const;
+    /* setters */
+    void setTarget(Target target);
+    void setApplicazione(std::string applicazione);
 };
 
 #endif // COSMETICO_H
