b6d3b8378c95b75fd77580477efa98572b1f085a
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-02 21:35:08 +0200

    Implementato operatore di assegnazione profonda

--- Container/smartp.cpp
+++ Container/smartp.cpp
@@ -11,3 +11,11 @@ SmartP::SmartP(const SmartP& s)
 SmartP::~SmartP() {
     if(p) delete p;
 }
+
+SmartP::SmartP& operator=(const SmartP& s) {
+  if(this != &s) {
+    if(p) delete p;
+    p = (s.p!=nullptr ? (s.p)->clone() : nullptr);
+  }
+  return *this;
+}
