b480174fe7700ba493618b2caccc7c1cd09d3702
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-02 16:27:23 +0200

    Indentato il codice

--- prodotto.h
+++ prodotto.h
@@ -15,28 +15,28 @@ std::string attributeToString(double d);
 */
 
 class Prodotto {
-private:
-  unsigned short _id;
-  std::string _nome;
-  std::string _descrizione;
-  double _prezzo;
-public:
-  Prodotto(unsigned short id, std::string nome, std::string descrizione, double prezzo=0.0);
-  virtual ~Prodotto();
-  virtual Prodotto* clone() const=0;
-  virtual double calcoloPrezzo() const=0;
-  /* da spostare in classe serializzazione */
-  // virtual std::string mostraProdotto() const=0;
-  /* getters */
-  unsigned short getId() const;
-  std::string getNome() const;
-  std::string getDescrizione() const;
-  double getPrezzo() const;
-  /* setters */
-  void setId(unsigned short id);
-  void setNome(std::string nome);
-  void setDescrizione(std::string descrizione);
-  void setPrezzo(double prezzo);
+  private:
+    unsigned short _id;
+    std::string _nome;
+    std::string _descrizione;
+    double _prezzo;
+  public:
+    Prodotto(unsigned short id, std::string nome, std::string descrizione, double prezzo=0.0);
+    virtual ~Prodotto();
+    virtual Prodotto* clone() const=0;
+    virtual double calcoloPrezzo() const=0;
+    /* da spostare in classe serializzazione */
+    // virtual std::string mostraProdotto() const=0;
+    /* getters */
+    unsigned short getId() const;
+    std::string getNome() const;
+    std::string getDescrizione() const;
+    double getPrezzo() const;
+    /* setters */
+    void setId(unsigned short id);
+    void setNome(std::string nome);
+    void setDescrizione(std::string descrizione);
+    void setPrezzo(double prezzo);
 };
 
 #endif // PRODOTTO_H
