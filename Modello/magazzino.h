bc2d8bc74e437d8228ceb5ddc7333ff27a0fbf1a
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-06 12:16:38 +0200

    Separato container dal modello magazzino

---
+++ Modello/magazzino.h
@@ -0,0 +1,17 @@
+/*
+#include <list>
+#include "smartp.h"
+#include "prodotto.h"
+
+class Magazzino {
+  private:
+    std::list<SmartP> sp;
+  public:
+    //inserisce un prodotto nel magazzino
+    bool insert(Prodotto* p);
+    //elimina il prodotto con un certo id
+    bool deleteProdotto(int id);
+    //funzione da rimuovere da qui
+    bool mostraProdotto(Prodotto* p);
+};
+*/
