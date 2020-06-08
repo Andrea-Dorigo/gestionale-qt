928f72470061470950db452c6168b03dd4ebdf58
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-08 18:38:47 +0200

    Modificato commento

--- Modello/magazzino.cpp
+++ Modello/magazzino.cpp
@@ -1,44 +1,42 @@
 #include "magazzino.h"
-/*
-#include "nlohmann/json.hpp"
-#include "serialize.h"
+// #include "nlohmann/json.hpp"
+// #include "serialize.h"
 
-bool Magazzino::insert(Prodotto* p) {
-  sp.push_back(p);
-  return true;
-}
-
-bool Magazzino::deleteProdotto(int id) {
-  Prodotto* q = nullptr;
-  auto it = sp.begin();
-  // finché la lista non finisce oppure trovi il prodotto
-  while(it != sp.end() || (*it)->getId() != id){
-    it++;
-  }
-  if((*it)->getId() == id){
-    it=sp.erase(it);
-    return true;
-  }
-  else return false;
-  // // metodo con ciclo for (meno efficiente)
-  // for (auto it = sp.begin(); it != sp.end(); it++){
-  //   if((*it)->getId() == id){
-  //     it=sp.erase(it);
-  //     //--it; non serve perche' tanto facciamo il return
-  //     return true;
-  //   }
-  //   else return false;
-  // }
-}
-
-bool Magazzino::mostraProdotto(Prodotto* p) {
-  Serialize s;
-  nlohmann::json prodotto;
-  prodotto["id"] = p->getId();
-  prodotto["nome"] = p->getNome();
-  prodotto["descrizione"] = p->getDescrizione();
-  prodotto["prezzo"] = p->getPrezzo();
-  s.Serialize::write("prodotto.json", prodotto);
-  return true;
-}
-*/
+// bool Magazzino::insert(Prodotto* p) {
+//   sp.push_back(p);
+//   return true;
+// }
+//
+// bool Magazzino::deleteProdotto(int id) {
+//   Prodotto* q = nullptr;
+//   auto it = sp.begin();
+//   // finché la lista non finisce oppure trovi il prodotto
+//   while(it != sp.end() || (*it)->getId() != id){
+//     it++;
+//   }
+//   if((*it)->getId() == id){
+//     it=sp.erase(it);
+//     return true;
+//   }
+//   else return false;
+//   // // metodo con ciclo for (meno efficiente)
+//   // for (auto it = sp.begin(); it != sp.end(); it++){
+//   //   if((*it)->getId() == id){
+//   //     it=sp.erase(it);
+//   //     //--it; non serve perche' tanto facciamo il return
+//   //     return true;
+//   //   }
+//   //   else return false;
+//   // }
+// }
+//
+// bool Magazzino::mostraProdotto(Prodotto* p) {
+//   Serialize s;
+//   nlohmann::json prodotto;
+//   prodotto["id"] = p->getId();
+//   prodotto["nome"] = p->getNome();
+//   prodotto["descrizione"] = p->getDescrizione();
+//   prodotto["prezzo"] = p->getPrezzo();
+//   s.Serialize::write("prodotto.json", prodotto);
+//   return true;
+// }
