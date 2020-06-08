b60d02a78cc4457115a3df59164fe19b87a85890
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-08 18:25:23 +0200

    Rimosso alcuni commenti

--- Modello/magazzino.h
+++ Modello/magazzino.h
@@ -11,9 +11,7 @@ class Magazzino {
     void save(const std::string&) const;
     void load(const std::string&);
     unsigned int count() const;
-    //inserisce un prodotto nel magazzino
     bool insert(Prodotto* p);
-    //elimina il prodotto con un certo id
     bool deleteProdotto(int id);
     //funzione da rimuovere da qui
     bool mostraProdotto(Prodotto* p);
