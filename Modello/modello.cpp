bce6b6f89de79b1fd8ab3229bf84fcb4785d8336
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-30 11:04:11 +0200

    Rinominato MAGENTA() -> visualizza()

--- Modello/modello.cpp
+++ Modello/modello.cpp
@@ -68,22 +68,20 @@ Container<SmartP<Prodotto>> Modello::filtra(std::string ditta, double costo) con
   return aux;
 }
 #include <iostream>
-void Modello::MAGENTA() const {
-  std::cout << magazzino.begin()->p->stampa() << std::endl;
-  std::cout << magazzino.end()->p->stampa() << std::endl;
-  // for(auto it = magazzino.begin(); it != magazzino.end(); it++) {
-  //   std::cout << it->p->stampa() << std::endl;
-  // }
+void Modello::visualizza() const {
+  for(auto it = magazzino.begin(); it != magazzino.end(); it++) {
+    std::cout << it->p->stampa() << std::endl;
+  }
 }
 
 int main() {
   Modello m1;
-  Cosmetico* c1 = new Cosmetico(1, "c1", "desc c1", 5.0, "Erbolario", UNISEX, "app c1");
-  Cosmetico* c2 = new Cosmetico(2, "c2", "desc c2", 10.0, "Erbolario", UNISEX, "app c2");
-  Cosmetico* c3 = new Cosmetico(3, "c3", "desc c3", 50.0, "Dardick", DONNA, "app c3");
+  Cosmetico* c1 = new Cosmetico(1, "c1", "desc c1", 5.0, "Erbolario1", 22, UNISEX, "app c1");
+  Cosmetico* c2 = new Cosmetico(2, "c2", "desc c2", 10.0, "Erbolario2", 22, UNISEX, "app c2");
+  Cosmetico* c3 = new Cosmetico(3, "c3", "desc c3", 50.0, "Erbolario3", 22, DONNA, "app c3");
   m1.insert(c1);
   m1.insert(c2);
   m1.insert(c3);
-  m1.MAGENTA();
+  m1.visualizza();
   return 0;
 }
